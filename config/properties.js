3391b508ed5ef9c8f6386b9141cbf84ca44a30e0
Andrew Michitson <michitson@yahoo.com>
2020-03-15 23:38:57 +0000

    logging

--- config/properties.js
+++ config/properties.js
@@ -1,3 +1,8 @@
+
+console.log('PORTT', process.env.PORT )
+console.log('DDBB', process.env.DB )
+
+
 module.exports = {
     PORT : process.env.PORT || 4000,
     DB : process.env.DB || `mongodb://localhost:27017/crud-mean`,
